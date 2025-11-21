0d3b873962095c6eed3f41cf1b6d052d76fc0122
nikishkaa <nikita.goutor@gmail.com>
2025-11-21 19:07:01 +0300

    Basic UI Upd.

--- app/register.tsx
+++ app/register.tsx
@@ -1,18 +1,44 @@
-import { View, StyleSheet, StatusBar} from "react-native";
-import { WebView } from 'react-native-webview';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import {View, StyleSheet, StatusBar, Platform} from "react-native";
+import {WebView} from 'react-native-webview';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import React, {useState} from "react";
 
 export default function AuthScreen() {
     const insets = useSafeAreaInsets();
+    const [scrollEnabled, setScrollEnabled] = useState(false); // на /login скролл отключён
+
     return (
-        <View style={{ flex: 1, paddingTop: insets.top, backgroundColor: 'black' }}>
-            <StatusBar barStyle="light-content" />
+        <View
+            style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? insets.top : 0,
+                backgroundColor: '#0A0A0A',
+            }}
+        >
+            <StatusBar barStyle="light-content" backgroundColor="#0A0A0A"/>
             <WebView
                 style={styles.container}
-                source={{ uri: 'http://192.168.1.104:8080/login\n' }}
-            />
-        </View>
+                source={{uri: 'http://192.168.1.104:8080/login'}}
+                overScrollMode="never"
+                bounces={false}
+                nestedScrollEnabled={false}
+                showsVerticalScrollIndicator={false}
+                // hideKeyboardAccessoryView={true}      // ← убирает белую полосу со стрелками на iOS
+                // keyboardDisplayRequiresUserAction={false} // (опционально) нормальное поведение показа клавиатуры
 
+                scrollEnabled={scrollEnabled}
+                onNavigationStateChange={(navState) => {
+                    const url = navState.url || "";
+                    // на /login скролл выключен, на остальных страницах включён
+                    if (url.endsWith("/login")) {
+                        setScrollEnabled(false);
+                    } else {
+                        setScrollEnabled(true);
+                    }
+                }}
+            />
+
+        </View>
     );
 }
 
@@ -20,7 +46,7 @@ export default function AuthScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#0a0a0a",
+        backgroundColor: "transparent",
     },
     content: {
         flex: 1,
