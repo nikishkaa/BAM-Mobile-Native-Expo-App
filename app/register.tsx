f4db44420ab90fbce9e3f44c3805026828ac9000
nikishkaa <nikita.goutor@gmail.com>
2025-11-26 14:37:30 +0300

    Basic UI Upd.

--- app/register.tsx
+++ app/register.tsx
@@ -1,79 +1,42 @@
-import {View, StyleSheet, StatusBar, Platform} from "react-native";
+import {View, StyleSheet, StatusBar} from "react-native";
 import {WebView} from 'react-native-webview';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import React, {useState} from "react";
 
 export default function AuthScreen() {
-    const insets = useSafeAreaInsets();
-    const [scrollEnabled, setScrollEnabled] = useState(false); // на /login скролл отключён
+    const [scrollEnabled, setScrollEnabled] = useState(false);
 
     return (
-        <View
-            style={{
-                flex: 1,
-                paddingTop: Platform.OS === 'ios' ? insets.top : 0,
-                backgroundColor: '#0A0A0A',
-            }}
-        >
-            <StatusBar barStyle="light-content" backgroundColor="#0A0A0A"/>
+        <View style={styles.root}>
+            <StatusBar hidden />
+
             <WebView
                 style={styles.container}
-                source={{uri: 'http://192.168.1.104:8080/login'}}
+                source={{uri: 'http://192.168.1.108:8080/home-page'}}
                 overScrollMode="never"
                 bounces={false}
                 nestedScrollEnabled={false}
                 showsVerticalScrollIndicator={false}
-                // hideKeyboardAccessoryView={true}      // ← убирает белую полосу со стрелками на iOS
-                // keyboardDisplayRequiresUserAction={false} // (опционально) нормальное поведение показа клавиатуры
 
-                scrollEnabled={scrollEnabled}
-                onNavigationStateChange={(navState) => {
-                    const url = navState.url || "";
-                    // на /login скролл выключен, на остальных страницах включён
-                    if (url.endsWith("/login")) {
-                        setScrollEnabled(false);
-                    } else {
-                        setScrollEnabled(true);
-                    }
-                }}
+                scrollEnabled={true}
+                // onNavigationStateChange={(navState) => {
+                //     const url = navState.url || "";
+                //
+                //     const isAuthScreen = /\/(login|register)(\?|$)/.test(url);
+                //
+                //     setScrollEnabled(!isAuthScreen);
+                // }}
             />
-
         </View>
     );
 }
 
-
 const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: '#0A0A0A',
+    },
     container: {
         flex: 1,
-        backgroundColor: "transparent",
+        backgroundColor: 'transparent',
     },
-    content: {
-        flex: 1,
-        padding: 16,
-        justifyContent: "center",
-        backgroundColor: "black",
-    },
-    title: {
-        fontSize: 33,
-        fontWeight: 'bold',
-        color: '#FFFFFF',
-        textAlign: 'center',
-        letterSpacing: -1,
-        includeFontPadding: false,
-        width: '100%',
-        lineHeight: 48,
-    },
-    lastWord: {
-        color: '#FF0000',
-    },
-    input: {
-        marginBottom: 16,
-    },
-    button: {
-        marginBottom: 8,
-    },
-    switchModeButton: {
-        marginBottom: 16,
-    },
-})
+});
